78e9de3268ff82060e3d1a22de7d2f6838a71a86
Tanapon Suwankesawong <mez.tanapon@hotmail.com>
2020-04-14 11:08:16 +0700

    Init

---
+++ Assets/Scripts/Test/EnemyTest.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTest : MonoBehaviour
+{
+    public float speed = 3.0f;
+
+    public int maxHealth = 5;
+    public float timeInvincible = 2.0f;
+
+    public int health { get { return currentHealth; } }
+
+    //test need to change to private and find in own room
+    public GameObject player;
+
+    int currentHealth;
+    bool isInvincible;
+    float invincibleTimer;
+
+    Rigidbody2D rigidbody2d;
+
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
+        currentHealth = maxHealth;
+    }
+
+    private void Update()
+    {
+        //ded
+        if (currentHealth == 0)
+        {
+            animator.SetTrigger("Fixed");
+            Destroy(gameObject);
+            return;
+            //or
+        }
+
+        if (player == null)
+            return;
+
+        //calculate from player pos
+        Vector2 playerPos = player.GetComponent<Rigidbody2D>().position;
+        Vector2 position = rigidbody2d.position;
+
+        float horizontal = playerPos.x - position.x;
+        float vertical = playerPos.y - position.y;
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Move X", lookDirection.x);
+        animator.SetFloat("Move Y", lookDirection.y);
+
+
+        position = position + move * speed * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+
+            //play hit animation
+
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        RubyController player = other.gameObject.GetComponent<RubyController>();
+
+        if (player != null)
+        {
+            player.ChangeHealth(-1);
+        }
+    }
+}
---
+++ Assets/Scripts/Test/RoomController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomController : MonoBehaviour
+{
+    public Cinemachine.CinemachineConfiner confiner;
+    public bool Lock = false;
+    public List<GameObject> lockObject;
+    public List<GameObject> enemies;
+
+    PolygonCollider2D polygon;
+
+    private void Start()
+    {
+        polygon = confiner.m_BoundingShape2D.GetComponent<PolygonCollider2D>();
+    }
+
+    private void Update()
+    {
+        //dev unlock
+        if (Input.GetKeyDown(KeyCode.Space))
+            Lock = false;
+
+        if (!Lock)
+        {
+            //test unlock camera
+            confiner.m_BoundingShape2D = polygon;
+            confiner.InvalidatePathCache();
+            foreach (GameObject obj in lockObject)
+                obj.SetActive(false);
+            Lock = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if (Lock)
+                //test lock camera
+                Debug.Log(collision.gameObject);
+                confiner.m_BoundingShape2D = GetComponent<PolygonCollider2D>();
+                confiner.InvalidatePathCache();
+                foreach (GameObject obj in lockObject)
+                    obj.SetActive(true);
+            foreach(GameObject enemy in enemies)
+            {
+                EnemyTest e = enemy.GetComponent<EnemyTest>();
+                if (e!= null)
+                {
+                    e.player = collision.gameObject;
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            foreach (GameObject enemy in enemies)
+            {
+                EnemyTest e = enemy.GetComponent<EnemyTest>();
+                if (e != null)
+                {
+                    e.player = null;
+                }
+            }
+        }
+    }
+}
