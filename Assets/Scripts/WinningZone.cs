f34f83d372c1c18038dd0d03ef655e2ce1fd556e
Satasuk Vipaksinlapin <satasuk.v@student.chula.ac.th>
2020-04-19 13:57:27 +0700

    add winning zone and UI

---
+++ Assets/Scripts/WinningZone.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinningZone : MonoBehaviour
+{
+    public GameObject MissionPassContainer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        RubyController controller = collision.GetComponent<RubyController>();
+        if (controller != null)
+        {
+            // winning
+            Debug.Log("win");
+            MissionPassContainer.SetActive(true);
+
+            // pause game use 1 to run game again
+            Time.timeScale = 0;
+        }
+
+    }
+}
