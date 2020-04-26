936220c99d169c77fce1fced1c23e7ea21665367
Kitipat Teachasupinun <Kitipat_Teacha@outlook.co.th>
2020-04-26 17:18:55 +0700

    add restart btn

---
+++ Assets/Scripts/ScenceController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ScenceController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Button restartBtn;
+    void Start()
+    {
+        Button btn = restartBtn.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick(){
+		Debug.Log ("load scence");
+        SceneManager.LoadScene(0);
+	}
+}
