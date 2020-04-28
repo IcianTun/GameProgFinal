7c7bb43969e069ab1c1c280c29740cde880aad16
Tanapon Suwankesawong <mez.tanapon@hotmail.com>
2020-04-28 16:03:42 +0700

    Implement MainMenuScene

---
+++ Assets/Scripts/SceneSelect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSelect : MonoBehaviour
+{
+    public void SelectScene()
+    {
+        switch (this.gameObject.name)
+        {
+            case "Scene1Button":
+                SceneManager.LoadScene("Stage1Scene");
+                break;
+            case "Scene2Button":
+                SceneManager.LoadScene("MezScene");
+                break;
+            case "Scene3Button":
+                SceneManager.LoadScene("TunScene");
+                break;
+            case "Scene4Button":
+                SceneManager.LoadScene("FightScene");
+                break;
+            case "Scene5Button":
+                SceneManager.LoadScene("ZezeScene");
+                break;
+        }
+    }
+}
