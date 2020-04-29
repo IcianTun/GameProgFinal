16694d4603bccf12d411d3bb63dfa68724634e39
Mawinw <time-whiter@hotmail.com>
2020-04-29 11:06:51 +0700

    merged

--- Assets/Scripts/Stage1Scripts/Lock.cs
+++ Assets/Scripts/Stage1Scripts/Lock.cs
@@ -6,7 +6,6 @@ public class Lock : NonPlayerCharacter
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("locker room enter");
         NonPlayerCharacter npc = gameObject.GetComponent<NonPlayerCharacter>();
         if(npc != null)
         {
